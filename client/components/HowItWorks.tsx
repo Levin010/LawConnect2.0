5a15f5cb309d202671e5d1a92b3416992038b331
Levin010 <levinomwenga10@gmail.com>
2026-04-14 15:46:08 +0300

    Update HowItWorks component to include chat and payment features

--- client/components/HowItWorks.tsx
+++ client/components/HowItWorks.tsx
@@ -1,19 +1,25 @@
 const steps = [
   {
-    title: '+ Create Account',
-    description:
-      "If looking for a lawyer to represent you in court proceedings, create a client account. If you're a certified advocate looking for clients, create an advocate account.",
-  },
-  {
     title: '+ Representation Requests',
     description:
-      'Client users can ask an advocate to represent them in legal proceedings by sending a representation request. Advocate users can either accept or reject a request. Accepting a request launches a new case.',
+      'Client users can ask an advocate to represent them in legal proceedings by sending a' + 
+      ' representation request. Advocate users can either accept or reject a request.' + 
+      ' Accepting a request launches a new case.',
   },
   {
-    title: '+ Case Updates',
+    title: '+ Case Updates & Chat',
     description:
-      'Once an advocate launches a case, they can post updates including documents on the case pages. The client attached to the case can view these updates, communicate with and pay their advocate through the platform.',
+      'Once an advocate launches a case, they can post case updates including documents on' + 
+      ' the case pages. The client can view these updates and communicate with their advocate' + 
+      ' through our secure chat feature.',
   },
+  {
+    title: '+ Pay Advocate',
+    description:
+      'After the case is resolved, the client can pay their advocate through our secure' + 
+      ' payment system. This can be done through M-Pesa or the various bank integrations we' + 
+      ' have e.g Equity Bank, NCBA Bank e.t.c.',
+  }
 ];
 
 export default function HowItWorks() {
